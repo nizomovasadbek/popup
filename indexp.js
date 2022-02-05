326a60c711447927ca2d304cfbfa32c2dc3b0e1d
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-02-05 14:47:19 +0500

    first commit

---
+++ indexp.js
@@ -0,0 +1,11 @@
+var b=0;
+    var menu=document.querySelector(".menyu");
+    function openclose(){
+    if(b==0){
+    menu.style.height="250px"
+    b=1;
+    }else{
+        menu.style.height="0px"
+        b=0;
+    }
+    } 
